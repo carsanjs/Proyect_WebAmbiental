b7a4e65ed9eb875f35f5f3e5ad358e4064eda0fb
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-06-20 08:16:26 -0500

    changes

--- Conection with Esp8266/Sensors Interno/config.h
+++
@@ -1,7 +0,0 @@
-const char* ssid     = "Device_IoT";
-const char* password = "Iot_2023C$";
-const char* hostname = "ESP8266Inner_S112";
-
-IPAddress ip(192, 168, 101, 200); //ELIMINADO ESTA LINEA TOMA UNA DIRRECIÓN IP AUTOMATICA
-IPAddress gateway(192, 168, 101, 1); //PUERTA DE ENLACE
-IPAddress subnet(255, 255, 255, 0);   //MASCARA DE SUBNET
